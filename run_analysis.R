b5d70f1190a2035d69e987196164e3cbaa7f07ff
abasaglia <abasaglia@users.noreply.github.com>
2014-06-16 20:34:50 +0200

    Update run_analysis.R
    Added a more complete script descrition

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,17 @@
+# Script run_analysis.R
+# Input
+#   train and test data from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+# Output
+#   1) tidy data set 1 with the mean and standard deviation for each measurement
+#   2) tidy data set with the average of each variable for each activity and each subject
+# Description
+# Merges the training and the test sets to create one data set.
+# Extracts only the measurements on the mean and standard deviation for each measurement. 
+# Uses descriptive activity names to name the activities in the data set
+# Appropriately labels the data set with descriptive variable names. 
+# Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+
 # Create directory, load the zip file and unzip it
 dirHAR = "./HARDataSet"
 if(!file.exists(dirHAR)) {dir.create(dirHAR)}
